d3b5a9995941d6ea34be1d7ebe25d7bf17388979
Loris <loris.kary67730@gmqil.com>
2024-01-15 10:44:26 +0100

    ajout lolo parsing

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -3,39 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkary-po <lkary-po@student.42.fr>          +#+  +:+       +#+        */
+/*   By: loris <loris@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 11:10:59 by loris             #+#    #+#             */
-/*   Updated: 2024/01/12 15:18:17 by lkary-po         ###   ########.fr       */
+/*   Updated: 2024/01/12 16:12:28 by loris            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+#include "stdbool.h"
+#include <stdio.h>
+
 bool	ft_is_sep(char c)
 {
-	char	symbols[] = "|<>()'\";&$";
+	// char	symbols[] = "|<>()'\";&$";
 	char	whitespace[] = " \t\r\n\v";
+	int		i;
 
-
+	i = -1;
+	while (whitespace[++i])
+	{
+		if (c == whitespace[i])
+			return (true);
+	}
+	return (false);
 }
 
 int	ft_countword(char *line)
 {
 
 	int		count;
+	int		i;
 
-	while ()
+	i = -1;
+	count = 0;
+	while (ft_is_sep(*line))
+		line++;
+	while (line[++i])
 	{
-		
+		if (ft_is_sep(line[i]) == false && (ft_is_sep(line[i + 1]) == true || line[i + 1] == '\0'))
+			count++;
 	}
+	return(count);
 }
 
 char    **ft_strtok(char *line)
 {
 	char	**list_token;
 
-	list_token =  our_malloc(sizeof(char *) * ft_countword(line));
+	list_token =  our_malloc(sizeof(char *) * (ft_countword(line) + 1));
+	list_token[ft_countword(line)] = NULL;
+	
 }
 
 int	parse_line(t_shell_data *shell_data, char *line)
