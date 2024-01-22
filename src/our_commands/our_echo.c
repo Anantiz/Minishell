dc50bdaa75ed1b0e7d2493d5eb535cb98f89610a
aurban <antoine.urban.68@gmail.com>
2024-01-22 11:14:23 +0100

    Broke everything again

--- src/our_commands/our_echo.c
+++ src/our_commands/our_echo.c
@@ -6,20 +6,11 @@
 /*   By: aurban <aurban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 14:41:43 by aurban            #+#    #+#             */
-/*   Updated: 2024/01/21 21:02:03 by aurban           ###   ########.fr       */
+/*   Updated: 2024/01/22 11:11:49 by aurban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-/*
-oooooooooo.
-`888'   `Y8b
- 888      888  .ooooo.  ooo. .oo.    .ooooo.
- 888      888 d88' `88b `888P"Y88b  d88' `88b
- 888      888 888   888  888   888  888ooo888
- 888     d88' 888   888  888   888  888    .o
-o888bood8P'   `Y8bod8P' o888o o888o `Y8bod8P'
-*/
 
 int	our_echo(t_shell_data *shell_data, t_s_token *node)
 {
