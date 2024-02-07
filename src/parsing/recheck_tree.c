39619e724a40681d7b2ca95b502d7327bc7e97b2
Loris Kary-Potes <lkary-po@k2r3p10.42mulhouse.fr>
2024-02-07 12:30:53 +0100

    NULL _ _ _ NULL

--- src/parsing/recheck_tree.c
+++ src/parsing/recheck_tree.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   recheck_tree.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aurban <aurban@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lkary-po <lkary-po@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 13:30:17 by loris             #+#    #+#             */
-/*   Updated: 2024/02/07 11:51:20 by aurban           ###   ########.fr       */
+/*   Updated: 2024/02/07 12:10:52 by lkary-po         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	replace_file(t_s_token *node)
 {
 	while (node)
 	{
-		if (node && node->token_type == TK_OP && (node->data.op.type == 4 || node->data.op.type == 5 || node->data.op.type == 6 || node->data.op.type == 7))
+		if (node && node->token_type == TK_OP && (node->data.op.type == REDIR_IN || node->data.op.type == REDIR_OUT || node->data.op.type == REDIR_APPEND))
 			node->right = new_tokenfile(node->right);
 		node = get_next_node(node);
 	}
@@ -26,12 +26,9 @@ void	replace_file(t_s_token *node)
 t_s_token *new_tokenfile(t_s_token *node)
 {
 	t_s_token	*new_node;
-	char	*t;
 
 	new_node = our_malloc(sizeof(t_s_token));
-	new_node->token_type = TK_FILE;
-	t = node->data.cmd.args[0];
-	new_node->data.file.file_path = t;
+	scan_tk_str_file(node->data.cmd.args[0], new_node);
 	new_node->parent = node->parent;
 	new_node->left = node->left;
 	new_node->right = node->right;
