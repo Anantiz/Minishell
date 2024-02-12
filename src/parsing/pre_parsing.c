ceeb7419ab83208937fdc4b9fa5546301680f1a4
Loris <loris.kary67730@gmqil.com>
2024-02-12 18:57:29 +0100

    redir parsing

---
+++ src/parsing/pre_parsing.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pre_parsing.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loris <loris@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/12 17:06:01 by loris             #+#    #+#             */
+/*   Updated: 2024/02/12 18:50:46 by loris            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+/*
+*   things to handle :
+*       -Unclosed quote
+		-Leading and Trailing pipes
+		-Trailing redir
+		-Uneven '&' or "&&&&" and if somehow there is "|||||" , do something with it
+*
+*/
+
+bool    pre_parsing(char **token_list)
+{
+	int i;
+
+	i = 0;
+	while (token_list[i])
+	{
+		
+	}
+}
+
+bool	function_checker(char *token)
+{
+
+}
+
+bool	leading_trailing_op(char **token_list)
+{
+	int	len;
+
+	if (scan_token(token_list[0])->token_type == TK_OP && !(scan_token(token_list[0])->data.op.type >= PIPE \
+		&& scan_token(token_list[0])->data.op.type <= REDIR_APPEND))
+		return (false);
+	len = ft_tablen(token_list);
+	if (scan_token(token_list[len - 1])->token_type == TK_OP)
+		return (false);
+	return (true);
+}
+
+bool	unclosed_quote(char *token)
+{
+	int	len;
+
+	len = ft_strlen(token);
+
+	if (token[0] == '\'')
+		if (token[len - 1] != '\'')
+			return (false);
+	if (token[0] == '"')
+		if (token[len - 1] != '"')
+			return (false);
+	return (true);
+}
+
+bool	parenthesis_checker(char **token_list)
+{
+	int	i;
+	int	count;
+
+	count = 0;
+	i = 0;
+	while (token_list[i])
+	{
+		if (ft_strncmp(token_list[i], "(") == 0)
+			count++;
+		if (ft_strncmp(token_list[i], ")") == 0)
+			count--;
+	}
+	if (count != 0)
+		return (false);
+	return (true);
+}
+
+bool	and_or_checker(char **token_list)
+{
+	int	i;
+
+	i = 0;
+	while (token_list[i])
+	{
+		if (token_list[i][0] == '|')
+		{
+			if (ft_strlen(token_list[i]) != 2)
+				return (false);
+			if (token_list[i][1] != '|')
+				return (false);
+		}
+		if (token_list[i][0] == '&')
+		{
+			if (ft_strlen(token_list[i]) != 2)
+				return (false);
+			if (token_list[i][1] != '&')
+				return (false);
+		}
+		i++;
+	}
+	return (true);
+}
+
+
+bool	unclosed_quote(char **token_list)
+{
+
+}
--- src/parsing/recheck_tree.c
+++ src/parsing/recheck_tree.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   recheck_tree.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aurban <aurban@student.42.fr>              +#+  +:+       +#+        */
+/*   By: loris <loris@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 13:30:17 by loris             #+#    #+#             */
-/*   Updated: 2024/02/12 14:20:43 by aurban           ###   ########.fr       */
+/*   Updated: 2024/02/12 16:49:15 by loris            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ t_s_token *new_tokenfile(t_s_token *node)
 	new_node = our_malloc(sizeof(t_s_token));
 	// to fix
 	if (!node->data.cmd.args)
-		ft_fprintf(2, "\033[31mnode->data.cmd.args is NULL\033[0m\n");
+		return (node);
 	scan_tk_str_file(node->data.cmd.args[0], new_node);
 	new_node->parent = node->parent;
 	new_node->left = node->left;
